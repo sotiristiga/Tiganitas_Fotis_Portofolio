e8d0fb9b333be39f8a907d6b1a1889d10404bf09
sotiristiga <99202968+sotiristiga@users.noreply.github.com>
2024-07-22 16:33:10 +0300

    Update InsuranceMarket.py

--- InsuranceMarket.py
+++ InsuranceMarket.py
@@ -8,7 +8,6 @@ import plotly.graph_objects as go
 import streamlit as st
 
 
-
 st.set_page_config(layout='wide',page_title="Insurance Market")
 IM_2020= pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/Testingapppanc/main/IM_2020.csv")
 IM_2021= pd.read_csv(f"https://raw.githubusercontent.com/sotiristiga/Testingapppanc/main/IM_2021.csv")
@@ -56,24 +55,28 @@ kpi3.metric(label="Καθαρά Ασφάλιστρα💶",
 kpi4.metric(label="Προμήθειες💶",
         value=IM['Commissions'].sum().round(2))
 
-tab1, tab2, tab3, tab4 = st.tabs(["Παραγωγή ανά εταιρεια","Παραγωγή ανά κλάδο", "Εξέλιξη Παραγωγής", "Δημογραφικά"])
+tab1, tab2, tab3, tab4 = st.tabs(["Παραγωγή ανά εταιρεια","Παραγωγή ανά κλάδο", "Εξέλιξη Παραγωγής", "Δημογραφικά Πελατών"])
 with tab1:
     tab11, tab12, tab13 = st.tabs(["Σύνολο Συμβολαίων", "Καθαρά", "Προμήθειες"])
-   
     with tab11:
         companies_countpol=IM1['Company'].value_counts().reset_index()
         fig_barplot=px.bar(companies_countpol,x='count',y='Company',title='',
                            labels={'count':'Σύνολο Συμβολαίων','Company':'Ασφ. Εταιρεία'},
-                        color_discrete_sequence= px.colors.sequential.Blugrn,text_auto=True)
-        fig_barplot.update_traces(textfont_size=17, textangle=0.5, textposition="outside", cliponaxis=False)
+                        color_discrete_sequence= px.colors.sequential.Blugrn,text_auto=True,
+                        height=1000)
+        fig_barplot.update_traces(textfont_size=17, textangle=0.5, textposition="outside", 
+                                  cliponaxis=False)
         fig_barplot.update_layout(plot_bgcolor='white',font_size=15)
         st.write(fig_barplot)
     with tab12:
         companies_net=IM1.groupby('Company')['Net'].sum().reset_index()
         companies_net_sorted=companies_net.sort_values('Net',ascending=True)
-        fig_barplot=px.bar(companies_net_sorted,x='Net',y='Company',title='',labels={'Net':'Καθαρά €','Company':'Ασφ. Εταιρεία'},
-                        color_discrete_sequence= px.colors.sequential.Blugrn,text_auto=True)
-        fig_barplot.update_traces(textfont_size=17, textangle=0, textposition="outside", cliponaxis=False)
+        fig_barplot=px.bar(companies_net_sorted,x='Net',y='Company',title='',
+                           labels={'Net':'Καθαρά €','Company':'Ασφ. Εταιρεία'},
+                        color_discrete_sequence= px.colors.sequential.Blugrn,
+                        text='Net',height=1000,range_x=[0,500000])
+        fig_barplot.update_traces(textfont_size=17,texttemplate = '%{text:.2s} €',
+                                   textangle=0, textposition="outside", cliponaxis=False)
         fig_barplot.update_layout(plot_bgcolor='white',font_size=15)
         st.write(fig_barplot)
     with tab13:
@@ -81,8 +84,9 @@ with tab1:
         companies_comm_sorted=companies_comm.sort_values('Commissions',ascending=True)
         fig_barplot=px.bar(companies_comm_sorted,x='Commissions',y='Company',title='',
                            labels={'Commissions':'Προμήθειες €','Company':'Ασφ. Εταιρεία'},
-                        color_discrete_sequence= px.colors.sequential.Blugrn,text_auto=True)
-        fig_barplot.update_traces(textfont_size=17, textangle=0, textposition="outside", cliponaxis=False)
+                        color_discrete_sequence= px.colors.sequential.Blugrn,
+                        text='Commissions',height=1000,range_x=[0,20000])
+        fig_barplot.update_traces(textfont_size=17, texttemplate = '%{text:.2s} €',textangle=0, textposition="outside", cliponaxis=False)
         fig_barplot.update_layout(plot_bgcolor='white',font_size=15)
         st.write(fig_barplot)
 with tab2:
@@ -91,7 +95,8 @@ with tab2:
         categories_countpol=IM1['Category'].value_counts().reset_index()
         fig_barplot=px.bar(categories_countpol,x='count',y='Category',title='',
                            labels={'count':'Σύνολο Συμβολαίων','Category':'Κλάδος'},
-                        color_discrete_sequence= px.colors.sequential.Blugrn,text_auto=True,range_x=[0,3000])
+                        color_discrete_sequence= px.colors.sequential.Blugrn,
+                        text_auto=True,range_x=[0,8000])
         fig_barplot.update_traces(textfont_size=17, textangle=0, textposition="outside", cliponaxis=False)
         fig_barplot.update_layout(plot_bgcolor='white',font_size=15)
         st.write(fig_barplot)
@@ -100,8 +105,9 @@ with tab2:
         categories_net_sorted=categories_net.sort_values('Net',ascending=True)
         fig_barplot=px.bar(categories_net_sorted,x='Net',y='Category',title='',
                            labels={'Net':'Καθαρά €','Category':'Κλάδος'},
-                        color_discrete_sequence= px.colors.sequential.Blugrn,text_auto=True,range_x=[0,100000])
-        fig_barplot.update_traces(textfont_size=17, textangle=0, textposition="outside", cliponaxis=False)
+                        color_discrete_sequence= px.colors.sequential.Blugrn,text='Net',
+                        range_x=[0,500000])
+        fig_barplot.update_traces(textfont_size=17, texttemplate = '%{text:.3s} €',textangle=0, textposition="outside", cliponaxis=False)
         fig_barplot.update_layout(plot_bgcolor='white',font_size=15)
         st.write(fig_barplot)
     with tab23:
@@ -109,20 +115,38 @@ with tab2:
         categories_comm_sorted=categories_comm.sort_values('Commissions',ascending=True)
         fig_barplot=px.bar(categories_comm_sorted,x='Commissions',y='Category',title='',
                            labels={'Commissions':'Προμήθειες €','Category':'Κλάδος'},
-                        color_discrete_sequence= px.colors.sequential.Blugrn,text_auto=True,range_x=[0,10000])
-        fig_barplot.update_traces(textfont_size=17, textangle=0, textposition="outside", cliponaxis=False)
+                        color_discrete_sequence= px.colors.sequential.Blugrn,text='Commissions',
+                        range_x=[0,30000])
+        fig_barplot.update_traces(textfont_size=17, texttemplate = '%{text:.2s} €',textangle=0, 
+                                  textposition="outside", cliponaxis=False)
         fig_barplot.update_layout(plot_bgcolor='white',font_size=15)
         st.write(fig_barplot)
 with tab3:
-    prod_line_by_year=IM.groupby('Month_Year')[['Commissions',"Net"]].sum().reset_index()
-    prod_line_by_year['Month_Year']=pd.to_datetime(prod_line_by_year['Month_Year'],format='mixed')
-    prod_line_by_year=prod_line_by_year.sort_values('Month_Year',ascending=False)
-    prod_line_by_year_count=IM['Month_Year'].value_counts().reset_index()
-    prod_line_by_year_count['Month_Year']=pd.to_datetime(prod_line_by_year_count['Month_Year'],format='mixed')
-    prod_line_by_year_count=prod_line_by_year_count.sort_values('Month_Year',ascending=False)
+    prod_line_by_month=IM.groupby('Month_Year')[['Commissions',"Net"]].sum().reset_index()
+    prod_line_by_month['Month_Year']=pd.to_datetime(prod_line_by_month['Month_Year'],format='mixed')
+    prod_line_by_month=prod_line_by_month.sort_values('Month_Year',ascending=False)
+    prod_line_by_month_count=IM['Month_Year'].value_counts().reset_index()
+    prod_line_by_month_count['Month_Year']=pd.to_datetime(prod_line_by_month_count['Month_Year'],format='mixed')
+    prod_line_by_month_count=prod_line_by_month_count.sort_values('Month_Year',ascending=False)
+    prod_line_by_month['Year']=prod_line_by_month['Month_Year'].dt.year
+    prod_line_by_month_count['Year']=prod_line_by_month_count['Month_Year'].dt.year
+    prod_line_by_year=prod_line_by_month.groupby('Year')[['Commissions',"Net"]].sum().reset_index()
+    prod_line_by_year_count=prod_line_by_month_count.groupby('Year')['count'].sum().reset_index()
+    prod_line_by_year_count['Year']=pd.Categorical(prod_line_by_year_count['Year'],pd.Series([2020,2021,2022,2023]))
     tab31, tab32, tab33 = st.tabs(["Σύνολο Συμβολαίων", "Καθαρά", "Προμήθειες"])
     with tab31:
-        fig_line_polcou = px.line(prod_line_by_year_count, 
+        fig_line_polcou = px.bar(prod_line_by_year_count, 
+                        x="Year", y="count", 
+                        title='Σύνολο συμβολαίων ανά έτος απο το 2020 έως 2023',
+                        color_discrete_sequence= px.colors.sequential.Aggrnyl,
+                        labels={'count':'Σύνολο συμβολαίων','Year':'Έτος'},
+                        range_y=[0,8000],width=500,text_auto=True)
+        fig_line_polcou.update_traces(textfont_size=17, textangle=0, 
+                                      textposition="outside", cliponaxis=False)
+        fig_line_polcou.update_layout(plot_bgcolor='white',font_size=15)
+        st.write(fig_line_polcou)
+        
+        fig_line_polcou = px.line(prod_line_by_month_count, 
                         x="Month_Year", y="count", 
                         title='Σύνολο συμβολαίων ανά μήνα απο το 2020 έως 2023',
                         color_discrete_sequence= px.colors.sequential.Aggrnyl,
@@ -131,18 +155,36 @@ with tab3:
         fig_line_polcou.update_layout(plot_bgcolor='white',font_size=13)
         st.write(fig_line_polcou)
     with tab32:
-        fig_line_net = px.line(prod_line_by_year, 
+        fig_line_polcou = px.bar(prod_line_by_year, 
+                        x="Year", y="Net", 
+                        title='Καθαρά ασφάλιστρα ανά έτος',
+                        color_discrete_sequence= px.colors.sequential.Aggrnyl,
+                        labels={'Net':'Καθαρά €','Year':'Έτος'},
+                        range_y=[0,230000],width=500,text='Net')
+        fig_line_polcou.update_traces(textfont_size=17,texttemplate = '%{text:.3s} €', textangle=0, textposition="outside", cliponaxis=False)
+        fig_line_polcou.update_layout(plot_bgcolor='white',font_size=15)
+        st.write(fig_line_polcou)
+        fig_line_net = px.line(prod_line_by_month, 
                         x="Month_Year", y="Net", 
-                        title='Κάθαρα Ασφάλιστρα ανά μήνα απο το 2020 έως 2023',
+                        title='Κάθαρα Ασφάλιστρα ανά μήνα ',
                         color_discrete_sequence= px.colors.sequential.Aggrnyl,
                         labels={'Net':'Καθαρά €','Month_Year':'Μήνας-Έτος'},
                         range_y=[0,35000],markers=True)
         fig_line_net.update_layout(plot_bgcolor='white',font_size=13)
         st.write(fig_line_net)
     with tab33:
-        fig_line_com = px.line(prod_line_by_year, 
+        fig_line_polcou = px.bar(prod_line_by_year, 
+                        x="Year", y="Commissions", 
+                        title='Προμήθειες ανά έτος',
+                        color_discrete_sequence= px.colors.sequential.Aggrnyl,
+                        labels={'Commissions':'Προμήθειες €','Year':'Έτος'},
+                        range_y=[0,30000],width=500,text='Commissions')
+        fig_line_polcou.update_traces(textfont_size=17,texttemplate = '%{text:.3s} €', textangle=0, textposition="outside", cliponaxis=False)
+        fig_line_polcou.update_layout(plot_bgcolor='white',font_size=15)
+        st.write(fig_line_polcou)
+        fig_line_com = px.line(prod_line_by_month, 
                         x="Month_Year", y="Commissions", 
-                        title='Προμήθειες ανά μήνα απο το 2020 έως 2023',
+                        title='Προμήθειες ανά μήνα',
                         color_discrete_sequence= px.colors.sequential.Aggrnyl,
                         labels={'Commissions':'Προμήθειες €','Month_Year':'Μήνας-Έτος'},
                         range_y=[0,3500],markers=True)
@@ -167,5 +209,33 @@ with tab4:
     ),legend_title_text='Φύλο',title_x=0.4)
     st.write(fig_pie)
     st.write("""* Σε αρκετα συμβόλαια δεν αναγραφόταν το φύλο του πελάτη. Επίσης, σε κάποια συμβόλαια υπάρχει συνιδιοκτησία""")
-
-
+    IM['District']=IM['District'].replace("ΑΙΤΩΛΟΑΚΑΡΝΑΝΙΑΣ","ΑΙΤΩΛΟΑΚΑΡΝΑΝΙΑ")
+    discrict_data=IM.groupby(['id','District'])[['Gross','Net','Commissions']].sum().reset_index()
+    discrict_data_total=IM.groupby(['District'])[['Gross','Net','Commissions']].sum().reset_index()
+    discrictcount=discrict_data['District'].value_counts().reset_index().sort_values('count')
+    tab41, tab42, tab43 = st.tabs(["Σύνολο Συμβολαίων", "Καθαρά", "Προμήθειες"])
+    with tab41:
+        fig_barplot_reg=px.bar(discrictcount,x='count',y='District',title='',
+                           labels={'count':'Αρ. Πελατών','District':'Νομός'},
+                        color_discrete_sequence= px.colors.sequential.Blugrn,
+                        text_auto=True,width=1200,height=1200,range_x=[0,1500])
+        fig_barplot_reg.update_traces(textfont_size=14, textangle=0.5, textposition="outside", cliponaxis=False)
+        fig_barplot_reg.update_layout(plot_bgcolor='white',font_size=25)
+        st.write(fig_barplot_reg)
+    with tab42:
+        fig_barplot_reg=px.bar(discrict_data_total.sort_values('Net'),x='Net',y='District',title='',
+                           labels={'Net':'Καθαρά €','District':'Νομός'},
+                        color_discrete_sequence= px.colors.sequential.Blugrn,text='Net',width=1000,
+                        height=1200,range_x=[0,600000])
+        fig_barplot_reg.update_traces(textfont_size=14,texttemplate = '%{text:.2s} €', textangle=0.5, textposition="outside", cliponaxis=False)
+        fig_barplot_reg.update_layout(plot_bgcolor='white',font_size=25)
+        st.write(fig_barplot_reg)
+    with tab43:
+        fig_barplot_reg=px.bar(discrict_data_total.sort_values('Commissions'),x='Commissions',y='District',title='',
+                           labels={'Commissions':'Προμήθειες €','District':'Νομός'},
+                        color_discrete_sequence= px.colors.sequential.Blugrn,text='Commissions',width=1200,height=1200,
+                        range_x=[0,70000] )
+        fig_barplot_reg.update_traces(textfont_size=14,texttemplate = '%{text:.3s} €', textangle=0.5, textposition="outside", cliponaxis=False)
+        fig_barplot_reg.update_layout(plot_bgcolor='white',font_size=25)
+        fig_barplot.update_xaxes(tickprefix="€")
+        st.write(fig_barplot_reg)
